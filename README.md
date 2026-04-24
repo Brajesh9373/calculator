645c4c06e529f94ec9bcd69e3b5c22e29fc3884f
Brajesh9373 <brajeshkurkure@gmail.com>
2026-04-24 14:22:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calculator Project
+
+This repository contains a simple calculator implementation. Two developers will work on separate features:
+
+- **Brajesh** – addition functionality (branch `addition`)
+- **Omesh** – division functionality (branch `division`)
---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""Simple calculator module.
+
+Provides basic arithmetic functions. Developers will extend this module
+on separate branches:
+- `addition` branch: implement `add(a, b)`
+- `division` branch: implement `divide(a, b)`
+"""
+
+def add(a, b):
+    """Return the sum of a and b. To be implemented on the `addition` branch."""
+    raise NotImplementedError("Addition not implemented yet")
+
+
+def divide(a, b):
+    """Return a divided by b. To be implemented on the `division` branch."""
+    raise NotImplementedError("Division not implemented yet")
